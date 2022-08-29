79b2f3aa202007df3b30f91f2a586f28b95084f9
Grant Robinson <Chisdeal2014@hotmail.com>
2022-08-30 00:17:37 +0100

    Update PromotedMember.jsx

--- src/app/components/elements/PromotedMember.jsx
+++ src/app/components/elements/PromotedMember.jsx
@@ -6,11 +6,65 @@ class PromotedMember extends Component {
         const { promoted_member, author } = this.props; // redux
 
         const promoted = promoted_member.get(author);
+        
+        let staff = '';
+        let developer = '';
+        let witness = '';
+        let contentcreator = '';
+        let officialblurtdevs = '';
+        let patreontag = '';
+        let promotedtag = '';
 
         if (promoted !== undefined) {
+            
             const description = `@${promoted.category}: ${promoted.description}`;
+            
+            if (promoted.isStaff == true) {
+                staff = {"css": `StaffMember`, "desc": `${description}`, "title": `STAFF`};
+            }
+            
+            if (promoted.isWitness == true) {
+                witness = {"css": `WitnessMember`, "desc": `${description}`, "title": `WITNESS`};
+            }
+            
+            if (promoted.isDev == true) {
+                developer = {"css": `DevMember`, "desc": `${description}`, "title": `ALLOY DEV`};
+            }
+            
+            if (promoted.isContentCreator == true) {
+                contentcreator = {"css": `CCMember`, "desc": `${description}`, "title": `CONTENT CREATOR`};
+            }
+            
+            if (promoted.isOfficialBlurtDev == true) {
+                officialblurtdevs = {"css": `DevMember`, "desc": `${description}`, "title": `BLURT DEV`};
+            }
+
+            if (promoted.isPromoted == true) {
+                promotedtag = {"css": `PromotedMember`, "desc": `${description}`, "title": `Promoted`};
+            }
+
+            if (promoted.patreon.isActive == true) {
+                if (promoted.patreon.tier == "DONATOR") {
+                    var cssname = "PatreonDonator";
+                } else if (promoted.patreon.tier == "PREMIUM") {
+                    var cssname = "PatreonPremium";
+                } else if (promoted.patreon.tier == "LEGEND") {
+                    var cssname = "PatreonLegend";
+                } else if (promoted.patreon.tier == "MEGA") {
+                    var cssname = "PatreonMEGA";
+                }
+
+                patreontag = {"css": cssname, "desc": `${description}`, "title": `Patreon: ${promoted.patreon.tier}`};
+            }
+            
+            const putalltogether = [staff,witness,developer,contentcreator,officialblurtdevs,promotedtag,patreontag];
+            
+            const listItems = putalltogether.map((d) => <span className={d.css} title={d.desc}>{d.title}</span>);
+        
             return (
-                <span className="PromotedMember" title={description}>Promoted - { promoted.category}</span>
+                <div>
+                {listItems}
+                </div>
             );
         }
         return null;
@@ -26,3 +80,4 @@ export default connect((state) => {
         promoted_member
     };
 })(PromotedMember);
+
