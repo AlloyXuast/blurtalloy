a905b7e4f2ce0e9cc948de27e5b76eae3d3f69ab
Grant Robinson <Chisdeal2014@hotmail.com>
2022-09-18 22:36:21 +0100

    Update ReplyEditor.jsx

--- src/app/components/elements/ReplyEditor.jsx
+++ src/app/components/elements/ReplyEditor.jsx
@@ -1310,15 +1310,15 @@ export default (formId) => connect(
             if (!isEdit) {
                 let messageHTML = '';
                 if (linkProps.parent_author && linkProps.parent_author.length > 0) {
-                    messageHTML = '<br /> <hr /> <center><sub>Posted from <a href="https://blurt.alloyxuast.tk'
+                    messageHTML = '<br /> <hr /> <center><sub>Posted from <a href="https://blurt.alloyxuast.co.uk'
                         + '/' + parent_permlink
-                        + '/@' + linkProps.parent_author + '/' + permlink + '">https://blurt.alloyxuast.tk</a></sub></center>';
+                        + '/@' + linkProps.parent_author + '/' + permlink + '">https://blurt.alloyxuast.co.uk</a></sub></center>';
                 } else {
-                    messageHTML = '<br /> <hr /> <center><sub>Posted from <a href="https://blurt.alloyxuast.tk'
+                    messageHTML = '<br /> <hr /> <center><sub>Posted from <a href="https://blurt.alloyxuast.co.uk'
                         // + '/' + parent_permlink
                         + '/@' + linkProps.author
                         // + '/' + permlink
-                        + '">https://blurt.alloyxuast.tk</a></sub></center>';
+                        + '">https://blurt.alloyxuast.co.uk</a></sub></center>';
                 }
 
                 if (!isStory) {
